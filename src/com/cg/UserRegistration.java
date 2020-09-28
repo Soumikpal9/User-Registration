6c00eefd67b20119ca312161feee49f858112a4f
Soumikpal9 <soumikpal1997@gmail.com>
2020-09-28 20:12:29 +0530

    Added: First Name Validated

--- src/com/cg/UserRegistration.java
+++ src/com/cg/UserRegistration.java
@@ -2,8 +2,28 @@ package com.cg;
 import java.util.*;
 import java.util.regex.*;
 
+//UC1
 public class UserRegistration {
+	public static void FirstNameValidation(String input) {
+		String regex = "^[A-Z]+[a-z A-Z]{2,}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(input);
+		boolean found = matcher.find();
+		if(found) {
+			System.out.println("First Name Validated");
+		}
+		else {
+			System.out.println("First Name Not Validated");
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration!!!");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter The First Name : ");
+		String firstName = sc.nextLine();
+		FirstNameValidation(firstName);
 	}
 }
